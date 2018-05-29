7c9ce1328556a6451a19cfc379bff85c228699ab
IreneGigabit <33124327+IreneGigabit@users.noreply.github.com>
2018-05-29 17:27:56 +0800

    WebAPI練習中

---
+++ WebAPIPrj/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPIPrj.Models {
+	public class Product {
+		public int ProductId { get; set; }
+		public string ProductName { get; set; }
+		public string ProductCategory { get; set; }
+		public decimal ProductPrice { get; set; }
+	}
+}
